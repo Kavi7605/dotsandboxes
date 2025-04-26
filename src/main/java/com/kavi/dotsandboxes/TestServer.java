f3709133e091f81d5fb37269f03b7fcda0bb2d47
Kavi7605 <110843467+Kavi7605@users.noreply.github.com>
2025-04-26 12:14:50 +0530

    Removing old Google login implementation and Firebase code

--- src/main/java/com/kavi/dotsandboxes/TestServer.java
+++
@@ -1,8 +0,0 @@
-package com.kavi.dotsandboxes;
-
-public class TestServer {
-    public static void main(String[] args) {
-        System.out.println("Starting test server...");
-        LocalHttpServer.startServer();
-    }
-}
